91ff5f4fc9cdba64d8efdce4dbf50c838ea98f2c
bel <bel@bel.home>
2024-06-11 14:12:31 +0200

    Frontend fixed

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -16,10 +16,10 @@ export const createTask = createAsyncThunk(
             const token = thunkAPI.getState().auth.user.token
             return await taskService.createTask(taskData, token)
         } catch (error) {
-            const message = (error.response && error.response.data && error.response.data.message) ||
-            error.message || 
-            error.toString()
-            return thunkAPI.reject
+            const message = (error.response && error.response.data && error.response.data.message) 
+            || error.message 
+            || error.toString()
+            return thunkAPI.rejectWithValue(message)
         }
     }
 )
